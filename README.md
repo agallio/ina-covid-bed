afeb984bf2d45cd7b5af0a9485d33b8efb8cd144
agallio <me@agallio.xyz>
2021-07-07 19:08:09 +0700

    Setup eslint & npm strict rules

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # ina-covid-bed
+
+## Prerequisites
+
+- Node > v14
+- pnpm
+
+## Develop
+
+```bash
+# install
+$ pnpm i
+
+# development build
+$ pnpm dev
+
+# production build
+$ pnpm build
+```
+
+## Contact
+
+[Twitter](https://twitter.com/agalliosamai)
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
@@ -21,12 +21,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
+          Powered by{' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  );
+  )
 }

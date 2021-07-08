91a6f2ff1b51b2b2e996db6f87db84728854ab79
Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-07-08 21:27:41 +0700

    change button background color

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,11 +3,18 @@ import { Box, IconButton, useColorMode } from '@chakra-ui/react'
 
 const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode()
+  const isDarkMode = colorMode === 'dark'
+  const bgButton = isDarkMode ? 'gray.700' : 'gray.100'
   return (
-    <Box pos="fixed" top={0} right={0} p={2}>
+    <Box pos="fixed" top={0} right={0} p={2} zIndex="modal">
       <IconButton
+        bg={bgButton}
+        _hover={{
+          bg: bgButton,
+          opacity: 0.9,
+        }}
         onClick={toggleColorMode}
-        icon={colorMode === 'light' ? <SunIcon /> : <MoonIcon />}
+        icon={isDarkMode ? <MoonIcon /> : <SunIcon />}
         aria-label="toggle color mode"
       />
     </Box>

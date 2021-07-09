c7759c7be73c05038f57302e8a2739f8a8841ee7
Yogi Wisesa <yogiwisesa@gmail.com>
2021-07-09 13:12:50 +0800

    refactor(map): convert to chakra, hospital list using HospitalCard
    refactor(map): convert to chakra, hospital list using HospitalCard
    
    refactor(map): convert to chakra, hospital list using HospitalCard

--- components/HospitalCard.js
+++ components/HospitalCard.js
@@ -17,7 +17,7 @@ function generateGoogleMapsLink(name) {
 }
 
 export default function HospitalCard(props) {
-  const { hospital } = props
+  const { hospital, onClick, onLocationClick } = props
 
   const lastUpdatedTime = getRelativeLastUpdatedTime(
     hospital.updated_at_minutes
@@ -28,6 +28,17 @@ export default function HospitalCard(props) {
   const bgNotAvailable = useColorModeValue('red.100', 'red.700')
   const bgDistanceAvailable = useColorModeValue('gray.200', 'gray.600')
   const bgDistanceNotAvailable = useColorModeValue('red.200', 'red.600')
+
+  const locationAction = {
+    ...(onLocationClick
+      ? {
+          onClick: onLocationClick,
+        }
+      : {
+          href: generateGoogleMapsLink(hospital.name),
+        }),
+  }
+
   return (
     <Box
       w="100%"
@@ -37,6 +48,7 @@ export default function HospitalCard(props) {
       borderRadius="md"
       shadow="md"
       p={4}
+      onClick={onClick}
     >
       <HStack alignItems="start" spacing="2">
         <VStack align="start" spacing="1" flex="3">
@@ -117,7 +129,7 @@ export default function HospitalCard(props) {
               target="_blank"
               rel="noreferrer"
               size="sm"
-              href={generateGoogleMapsLink(hospital.name)}
+              {...locationAction}
             >
               <span style={{ marginRight: 5 }} aria-label="peta">
                 📍
